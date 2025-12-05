cfff68faf6a955095825190b46d8427073d83430
thesmalllest <vikulik.vs.137@gmail.com>
2025-12-05 22:39:01 +0300

    task 4

--- lab_4/lab_4.py
+++ lab_4/lab_4.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-# task 1 2 3
+# task 1 2 3 4
 def non_max_suppression(magnitude, angle):
     H, W = magnitude.shape
     suppressed = np.zeros((H, W), dtype=np.float32)
@@ -42,6 +42,40 @@ def non_max_suppression(magnitude, angle):
 
     return suppressed
 
+def double_threshold(img, low_ratio=0.05, high_ratio=0.15):
+    high = img.max() * high_ratio
+    low = high * low_ratio
+
+    H, W = img.shape
+    res = np.zeros((H, W), dtype=np.uint8)
+
+    strong = 255
+    weak = 80
+
+    strong_y, strong_x = np.where(img >= high)
+    weak_y, weak_x = np.where((img <= high) & (img >= low))
+
+    res[strong_y, strong_x] = strong
+    res[weak_y, weak_x] = weak
+
+    return res, weak, strong
+
+def hysteresis(img, weak=80, strong=255):
+    H, W = img.shape
+
+    for y in range(1, H - 1):
+        for x in range(1, W - 1):
+            if img[y, x] == weak:
+                # Если хотя бы один сосед сильный — делаем пиксель сильным
+                if (img[y+1, x-1] == strong or img[y+1, x] == strong or img[y+1, x+1] == strong or
+                    img[y,   x-1] == strong or img[y,   x+1] == strong or
+                    img[y-1, x-1] == strong or img[y-1, x] == strong or img[y-1, x+1] == strong):
+                    img[y, x] = strong
+                else:
+                    img[y, x] = 0  
+
+    return img
+
 def process_image(path):
     img = cv2.imread(path)
 
@@ -61,12 +95,20 @@ def process_image(path):
     # supression
     suppressed = non_max_suppression(magnitude, angle)
 
+    # double threshold
+    dt, weak, strong = double_threshold(suppressed)
+
+    # hysteresis
+    final_edges = hysteresis(dt, weak, strong)
+
     mag_norm = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
     sup_norm = cv2.normalize(suppressed, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
 
     cv2.imshow("B&W", gray)
     cv2.imshow("Grad magnitute", mag_norm)
     cv2.imshow("After Non-Max Suppression", sup_norm)
+    cv2.imshow("Double Threshold", dt)
+    cv2.imshow("Edges After Hysteresis", final_edges)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
